dc16e0c7d83071db76d4881fb23e6f41a316cba1
Achille-Tâm GUILCHARD <achilletamguilchard@gmail.com>
2023-04-06 16:21:55 +0200

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM nvidia/cuda:12.0.1-cudnn8-devel-ubuntu20.04
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+            gcc                            \
+            git                            \
+            python3                        \
+            python3-dev                    \
+            python3-pip                    \  
+            python3-opencv              && \
+            apt-get autoremove          && \          
+            apt-get clean               && \                
+            rm -rf /var/lib/apt/lists/* && \
+            pip3 install --no-cache-dir    \
+            pip                            \
+            setuptools                  && \
+            python3 -m pip install --upgrade pip
+
+RUN git clone https://github.com/facebookresearch/segment-anything.git && \
+    cd segment-anything && \
+    pip3 install -e .
+
+RUN pip3 install torch torchvision opencv-python pycocotools matplotlib onnxruntime onnx
---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import numpy as np
+import torch 
+import torchvision
+import time
+import matplotlib.pyplot as plt
+import cv2
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+print("PyTorch version:     ", torch.__version__)
+print("Torchvision version: ", torchvision.__version__)
+print("CUDA is available:   ", torch.cuda.is_available())
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    polygons = []
+    color = []
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        img = np.ones((m.shape[0], m.shape[1], 3))
+        color_mask = np.random.random((1, 3)).tolist()[0]
+        for i in range(3):
+            img[:,:,i] = color_mask[i]
+        ax.imshow(np.dstack((img, m*0.35)))
+
+
+def draw_anns(masks, image, output_path="result.png"):
+    plt.figure(figsize=(20,20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.savefig(output_path)
+
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--image_path", type=str, default="1.jpg")
+argparser.add_argument("--checkpoint", type=str, default="sam_vit_h_4b8939.pth")
+args = argparser.parse_args()
+
+# Delete the folder if it exists
+if os.path.exists("results"):
+    os.system("rm -rf " + "results")
+# Create the folder again
+os.makedirs("results")
+
+sam_checkpoint = args.checkpoint
+image_path     = args.image_path
+image          = cv2.imread(image_path)
+image          = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_bgr      = cv2.imread(image_path)
+
+device     = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_type = "default"
+sam        = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+start = time.time()
+masks = mask_generator.generate(image)
+end = time.time()
+print("Elapsed time (inference): {} secs".format(round(end - start, 2)))
+
+start = time.time()
+draw_anns(masks, image, output_path="results/result.png")
+end = time.time()
+print("Elapsed time (draw annotations): {} secs".format(round(end - start, 2)))
